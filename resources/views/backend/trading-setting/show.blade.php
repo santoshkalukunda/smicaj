39f2a0fdc370b45b71bbd0e79540697d6e2f9f6f
santoshkalukunda <santoshkalukunda@gmail.com>
2023-02-03 13:19:42 +0545

    show page title

--- resources/views/backend/trading-setting/show.blade.php
+++ resources/views/backend/trading-setting/show.blade.php
@@ -1,8 +1,30 @@
 @extends('layouts.app')
 @section('content')
     <!-- Content Header (tradingSetting header) -->
+    @php
+    if($tradingSetting->trading == "intraday_trading"){
+        $trading = "Intraday Trading";
+    }elseif ($tradingSetting->trading == "swing_trading") {
+        $trading = "Swing Trading";  
+    }else{
+        $trading = "Long Term Trading";  
+    }
+    if($tradingSetting->trading_time == "very_short_term"){
+        $trading_time = "Very Short term";
+    }elseif ($tradingSetting->trading_time == "short_term") {
+        $trading_time = "Short Term";  
+    }else{
+        $trading_time = "Long Term";  
+    }
+    if($tradingSetting->trading_time == true){
+        $buy_sell = "Buy";
+    }else{
+        $buy_sell = "Sell";  
+    }
+
+    @endphp
     <div class="row py-2">
-        <h3 class=" text-center"> {{ $title = 'Trading' }} </h3>
+        <h3 class=" text-center"> {{ $title = $trading ." In ". $trading_time ." For " . $buy_sell }} </h3>
     </div><!-- /.row -->
     <div class="row">
         <!-- Left col -->
